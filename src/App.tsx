2525c1416630f97e97a1d1bc11f913255e38bc7c
Farouk-Osman <faroukosman53@gmail.com>
2025-08-12 21:27:56 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -116,8 +116,7 @@ function App() {
             </div>
             <p className="text-xl md:text-2xl text-starWhite/90 mb-8 animate-fade-in opacity-0 [animation-delay:1000ms] max-w-2xl mx-auto leading-relaxed">
               I specialize in building robust backend systems using Node.js, Express.js, Nestjs and various database technologies. My passion lies in creating efficient, scalable, and secure applications that enhance user experiences.
-              <br />
-              <br />
+              
               Let's connect and explore how I can contribute to your next project!
             </p>
             <div className="flex gap-6 justify-center animate-fade-in opacity-0 [animation-delay:1200ms]">
